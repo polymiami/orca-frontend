8df7533f1ccb10f713e7cb53e8ca40e1d8a657e5
orcinuslabs <info@orcinuslabs.ca>
2021-05-29 07:46:51 -0700

    perf: setup RPC rotation

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -3,9 +3,20 @@ import random from 'lodash/random'
 // Array of available nodes to connect to
 const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
 
+/*
+ * Split 60/40 between Matic Vigil/Infura to use up entire allotment 
+ * of free tiers. 
+ * 
+ * Free Tiers:
+ * ============
+ * Matic Vigil  -> 150,000 requests/day (60%)
+ * Infura       -> 100,000 requests/day (40%)
+ */
 const getNodeUrl = () => {
-  const randomIndex = random(0, nodes.length - 1)
-  return nodes[randomIndex]
+  if (Math.random() < 0.6) {
+    return nodes[0]  
+  }
+  return nodes[1]
 }
 
 export default getNodeUrl
